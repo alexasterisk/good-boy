42d91f5a6d70c7faeed580ebd09c0ca18a35a989
alex* <31458482+alexasterisk@users.noreply.github.com>
2023-06-15 01:03:08 -0400

    add type returns to keyv

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,3 +1,17 @@
 import Keyv from 'keyv';
 
-export const keyv = new Keyv('sqlite://data/keyv.sqlite');
+export const raw = new Keyv('sqlite://data/keyv.sqlite');
+
+export const keyv = {
+    get: async <T>(key: string): Promise<T | undefined> => {
+        return await raw.get(key);
+    },
+
+    set: async <T>(key: string, value: T): Promise<boolean> => {
+        return await raw.set(key, value);
+    },
+
+    delete: async (key: string): Promise<boolean> => {
+        return await raw.delete(key);
+    }
+};
